5edc52b8e92f0d9e7ea917c84337c013f2237243
James <James@nodeloc.com>
2024-01-18 12:05:28 +0800

    init

--- migrations/2023_01_10_000001_add_user_money_index.php
+++ migrations/2023_01_10_000001_add_user_money_index.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Schema\Builder;
 return [
     'up' => function (Builder $schema) {
         $schema->table('users', function(Blueprint $table){
-            $table->index('money');
+            $table->index('last_checkin_money');
         });
     },
     'down' => function (Builder $schema) {
-        
+
     },
 ];
